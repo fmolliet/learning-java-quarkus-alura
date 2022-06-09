452e0f1ef359e1b3ccf5983f2a4b8dbbb1c6ee4a
fmolliet <fmolliet@hotmail.com>
2022-06-08 22:05:59 -0300

    Adicionando as regras de usuario para criação e adicionando Security JPA

--- src/main/java/br/com/alura/model/Usuario.java
+++ src/main/java/br/com/alura/model/Usuario.java
@@ -5,14 +5,23 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.wildfly.security.password.interfaces.BCryptPassword;
+
+import io.quarkus.elytron.security.common.BcryptUtil;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import io.quarkus.security.jpa.Password;
+import io.quarkus.security.jpa.Roles;
+import io.quarkus.security.jpa.UserDefinition;
+import io.quarkus.security.jpa.Username;
 
 // Acesso a base de dados como Active Record
 @Entity
 @Data
 @EqualsAndHashCode(callSuper=false)
+@UserDefinition
 public class Usuario extends PanacheEntityBase{
     
     @Id
@@ -22,8 +31,23 @@ public class Usuario extends PanacheEntityBase{
     private String nome;
     
     private String cpf;
-    
+    @Username
     private String username;
-    
+    @Password
     private String password;
+    @Roles
+    private String role;
+    
+    public static void adicionar ( Usuario usuario ){
+        usuario.password = BcryptUtil.bcryptHash(usuario.password);
+        usuario.role = validarUsername(usuario.username);
+        usuario.persist();
+    }
+    
+    private static String validarUsername( String username ){
+        if ( "alura".equals(username) ){
+            return "admin";
+        } 
+        return "user";
+    }
 }
