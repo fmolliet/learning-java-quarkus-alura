4edb921e3caabf82a512e9e17b5c46c5a05969b5
fmolliet <fmolliet@hotmail.com>
2022-06-14 13:21:21 -0300

    Adicionmado conteudo da aula 4 - segurança na aplicação com quarkus

--- src/main/java/br/com/alura/resource/OrdemResource.java
+++ src/main/java/br/com/alura/resource/OrdemResource.java
@@ -1,28 +1,28 @@
 package br.com.alura.resource;
 
-import java.time.LocalDate;
-
+import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
 
 import br.com.alura.model.Ordem;
-import br.com.alura.repository.OrdemRepository;
+import br.com.alura.service.OrdemService;
 
 @Path("/ordens")
 public class OrdemResource {
-    @Inject OrdemRepository repository;
-    
+    @Inject
+    OrdemService service;
+
     @POST
     @Transactional
+    @RolesAllowed("user")
     @Consumes(MediaType.APPLICATION_JSON)
-    public void inserir(Ordem ordem){
-        ordem.setData(LocalDate.now());
-        ordem.setStatus("ENVIADA");
-        
-        repository.persist(ordem);
+    public void inserir(@Context SecurityContext securityContext, Ordem ordem) {
+        service.inserir(securityContext, ordem);
     }
 }
---
+++ src/main/java/br/com/alura/service/OrdemService.java
@@ -0,0 +1,33 @@
+package br.com.alura.service;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.core.SecurityContext;
+
+import br.com.alura.model.Ordem;
+import br.com.alura.model.Usuario;
+import br.com.alura.repository.OrdemRepository;
+import io.quarkus.security.ForbiddenException;
+
+@ApplicationScoped
+public class OrdemService {
+
+    @Inject OrdemRepository repository;
+    
+    public void inserir(SecurityContext securityContext, Ordem ordem) {
+        Optional<Usuario> usuarioOptional = Usuario.findByIdOptional(ordem.getUserId());
+        Usuario usuario = usuarioOptional.orElseThrow();
+        
+        if (!usuario.getUsername().equals(securityContext.getUserPrincipal().getName())){
+            throw new ForbiddenException("O usuário é diferente do enviado!");
+        }
+        
+        ordem.setData(LocalDate.now());
+        ordem.setStatus("ENVIADA");
+        repository.persist(ordem);
+    }
+
+}
